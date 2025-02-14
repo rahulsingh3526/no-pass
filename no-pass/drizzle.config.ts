4a364925bdb178efc89229a4503aa17c1d3a2a56
Rahul Singh <koderrahul@gmail.com>
2025-02-14 09:39:26 +0530

    changing src of database

--- no-pass/drizzle.config.ts
+++ no-pass/drizzle.config.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
